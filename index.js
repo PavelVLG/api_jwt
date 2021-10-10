379c028c63c4f52942c89cf4bc9bcca108695511
Pavelvlg <pavelvlg444@gmail.com>
2021-10-10 13:23:39 +0300

    add mongo setting

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from "express";
-
-const PORT = process.env.PORT ?? 8000;
+import config from "config";
+import mongoose from "mongoose";
 
 const app = express()
+const PORT = config.get('port') || 8000;
+const MONGOURL = config.get('mongoUrl');
 
-app.listen(PORT, ()=>{
-    console.log(`server started on port: ${PORT}`)
+
+app.get('/', (req,res)=>{
+    res.send(`<h1>server started on port: ${PORT} </h1>`)
 })
+
+
+async function start(){
+    try{
+        await mongoose.connect(MONGOURL, {
+            useNewUrlParser: true,
+        })
+        app.listen(PORT, ()=>{
+            console.log(`server started on port: ${PORT}`)
+        })
+    }catch (e) {
+        console.log(`server has error ${e.message}`)
+        process.exit(1)
+    }
+}
+start()
