9a4380369dcf92d82eb30691c18abf2901b16d62
Pavelvlg <pavelvlg444@gmail.com>
2021-10-15 18:05:58 +0300

    added registrations

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,22 @@
 const User = require('../models/User')
 const Role = require('../models/Role')
+const bcrypt = require('bcrypt')
 
 class AuthController {
     async registrations(req, res) {
         try {
-
+            const {userName, password} = req.body;
+            const candidate = await User.findOne({userName})
+            if (candidate){
+                return res.status(400).json( {message: 'Это имя занято'})
+            }
+            const hashPassword = bcrypt.hashSync(password, 6)
+            const userRole = await Role.findOne({value: "USER"})
+            const user = new User({userName , password: hashPassword, roles:[userRole.value]})
+            await user.save()
+            return res.json({message: 'Успешная регистрация'})
         } catch (err) {
-
+            res.status(400).json({message: 'registrations error'})
         }
     }
 
@@ -14,7 +24,7 @@ class AuthController {
         try {
 
         } catch (err) {
-
+            res.status(400).json({message: 'login error'})
         }
     }
 
@@ -22,7 +32,7 @@ class AuthController {
         try {
             res.json('Your have this message because your connect with server api')
         } catch (err) {
-
+            res.status(400).json({message: 'getUser error'})
         }
     }
 }
