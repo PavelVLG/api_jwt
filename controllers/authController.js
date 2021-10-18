a15bb348ec7d751230e2a4a679a98effa2abaeba
Pavelvlg <pavelvlg444@gmail.com>
2021-10-18 16:16:43 +0300

    bag fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,28 @@
 const User = require('../models/User')
 const Role = require('../models/Role')
 const bcrypt = require('bcrypt')
+const {log} = require("nodemon/lib/utils");
 
 class AuthController {
     async registrations(req, res) {
         try {
-            const {userName, password} = req.body;
-            const candidate = await User.findOne({userName})
-            if (candidate){
-                return res.status(400).json( {message: 'Это имя занято'})
+            const {username, password} = await req.body;
+            const candidate = await User.findOne({username})
+            console.log("candidate:", candidate)
+            if (candidate != null) {
+                if (candidate.username === username) {
+                    return res.status(400).json({message: `Пользователь с  именем ${username} уже существует`})
+                }
+            } else {
+                const hashPassword = await bcrypt.hashSync(password, 7);
+                const userRole = await Role.findOne({value: "USER"})
+                const user = new User({username, password: hashPassword, roles: [userRole.value]})
+                await user.save()
+                return await res.json({message: "Пользователь успешно зарегистрирован"})
             }
-            const hashPassword = bcrypt.hashSync(password, 6)
-            const userRole = await Role.findOne({value: "USER"})
-            const user = new User({userName , password: hashPassword, roles:[userRole.value]})
-            await user.save()
-            return res.json({message: 'Успешная регистрация'})
-        } catch (err) {
-            res.status(400).json({message: 'registrations error'})
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({message: 'Registration error'})
         }
     }
 
--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
+const authUser = require('./routers/users')
 require('dotenv/config')
 const PORT = process.env.PORT || 8000;
 const DB_CONNECT = process.env.DB_CONNECTION_REMOTE;
-
-
 
 
 const app = express();
@@ -15,7 +14,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-const authUser = require('./routers/users')
 app.use('/api', authUser)
 
 
