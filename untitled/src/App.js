013716a1753ad8618fef1442727a2a54a396b02a
Pavelvlg <pavelvlg444@gmail.com>
2021-10-18 11:39:21 +0300

    added index

--- untitled/src/App.js
+++ untitled/src/App.js
@@ -5,22 +5,22 @@ import {qwestions} from './mock';
 
 function App() {
     const [state, setState] = useState([...qwestions])
-    const [view, setView] = useState(() => state[state.length - 1])
+    const [view, setView] = useState( state[state.length - 1])
     const [answerVisibility, setAnswerVisibility] = useState(false)
 
     const repeat = () => {
         setView(state[Math.floor(Math.random() * state.length)])
+        console.table(state)
+        console.log(view)
     }
     const success = async () => {
 
         if (state.length > 1) {
             setState(state.filter(item => item !== view))
             repeat()
-
         } else {
             setView(['ok', 'ok'])
         }
-
     }
 
 
@@ -28,7 +28,7 @@ function App() {
         return (
             <div style={{border: '1px solid black', maxWidth: '200px', margin: '0 auto', borderRadius: '5px', padding: '3px 3px 3px 3px'}}>
                 <p>
-                    {view[0]}
+                   <span style={{fontWeight: 'bold'}}>{state.indexOf(view) +1}</span> :   {view[0]}
                 </p>
             </div>
         )
@@ -43,6 +43,7 @@ function App() {
     }
 
     useEffect(() => {
+
         setAnswerVisibility(false)
         console.table(state)
         console.log(view)
@@ -60,6 +61,7 @@ function App() {
             <div style={{marginTop: "16px"}}>
                 <Button variant="contained" onClick={() => setAnswerVisibility(true)}>ответ</Button>
             </div>
+            <div><h2>кол-во вопросов: {state.length}</h2></div>
             <div style={{marginTop: "34px"}}>
                 <Question/>
             </div>
